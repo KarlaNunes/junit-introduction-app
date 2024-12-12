aeae3b163d60eb0a4636e8695d3e7f314bd43dd7
KarlaNunes <karla.julyana1@gmail.com>
2024-12-12 15:21:48 -0300

    :tada: initial setup

---
+++ src/main/java/com/ifrn/junitintroductionapp/JunitIntroductionAppApplication.java
@@ -0,0 +1,13 @@
+package com.ifrn.junitintroductionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitIntroductionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitIntroductionAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifrn/junitintroductionapp/JunitIntroductionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifrn.junitintroductionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitIntroductionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
